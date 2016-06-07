e8e53ada1edb5b9461b87bac2ea10e7ef5c6be10
Koichi Okada <kokada18@outlook.com>
2016-06-06 17:50:03 -0700

    DJ's project submission

---
+++ grouproject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  grouproject
+//
+//  Created by profile on 6/1/16.
+//  Copyright © 2016 DeAnza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "go15.png")!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
